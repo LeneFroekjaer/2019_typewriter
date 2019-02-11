125762b16108a7943314f3b17be5975c774358f5
Lene Frøkjær <lenefroekjaer@Lenes-MacBook-Pro-2.local>
2019-02-11 19:21:18 +0100

    typewriter - the main part

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,22 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+const typewriter = document.querySelector("#typewriter").innerHTML;
+let counter = typewriter.length;
+
+function init() {
+  document.querySelector("#typewriter").innerHTML = "";
+  typewriterloop((counter = 0));
+}
+
+function typewriterloop() {
+  document.querySelector("#typewriter").innerHTML = typewriter.slice(
+    0,
+    counter
+  );
+  counter++;
+
+  if (typewriter.length + 1 > counter) {
+    window.setTimeout(typewriterloop, 400);
+  }
+}
