01da7eba2c870013966d71159ae6da36460d8333
Lene Frøkjær <lenefroekjaer@Lenes-MacBook-Pro-2.local>
2019-02-11 21:32:54 +0100

    Sound on spaces and letters. not sure if it works when reloading the page

--- typewriter.js
+++ typewriter.js
@@ -10,10 +10,25 @@ function init() {
 }
 
 function typewriterloop() {
+  // FOR SOUNDS
+  let lastCharacter = typewriter.charAt(counter - 1);
+  const soundSpace = document.querySelector("#typespace");
+  const soundLetter = document.querySelector("#typekey2");
+  //
+
   document.querySelector("#typewriter").innerHTML = typewriter.slice(
     0,
     counter
   );
+
+  // FOR SOUNDS
+  if (lastCharacter == "") {
+    soundSpace.play();
+  } else {
+    soundLetter.play();
+  }
+  //
+
   counter++;
 
   if (typewriter.length + 1 > counter) {
